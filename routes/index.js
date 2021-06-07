815a4730302e0a67015233ae80d723591d781622
Felipe Morales <felipe.morales.querol@gmail.com>
2021-06-07 08:16:11 -0300

    Actualización de comentarios de rutas

--- routes/index.js
+++ routes/index.js
@@ -7,29 +7,20 @@ const userController = require('../controllers/usersController');
 const localidadesController= require('../controllers/localidadesController');
 
 module.exports = function (){
-    // post: /contribuyentes
+    // contribuyentes
     router.post('/contribuyentes', contribuyentesController.add);
-    // get: /contribuyentes
     router.get('/contribuyentes', contribuyentesController.list);
-    // get: /contribuyentes/:id
     router.get('/contribuyentes/:id', contribuyentesController.show);
     
     // localidades
-    // post: 
     router.post('/localidades', localidadesController.add);
-    // get: 
     router.get('/localidades', localidadesController.list);
-    // get: 
     router.get('/localidades/:id', localidadesController.show);  
 
     // user
-    // post: 
     router.post('/user', userController.add);
-    // get: 
     router.get('/user', userController.list);
-    // get: 
     router.get('/user/:id', userController.show); 
-   
 
     return router;
 };
